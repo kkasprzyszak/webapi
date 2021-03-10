af2907488196fe9bbbdecbe4cc5dd47649d05f4b
Katarzyna Kasprzyszak <kkasp24@gmail.com>
2021-03-10 16:51:55 +0000

    first commit

---
+++ DataAccess/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+
+        List<T> Get();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Remove(T entity);
+    }
+}
---
+++ DataAccess/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using Milennium.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// todo: class is not thread safe
+    /// </summary>
+    public class ProductRepository : IRepository<Product>
+    {
+        private List<Product> _products = new List<Product>();
+
+        public ProductRepository()
+        {
+            _products.Add(new Product() { Id = 1, Name = "Monitor", Price = 1500m });
+
+            _products.Add(new Product() { Id = 2, Name = "Computer", Price = 7500m });
+
+            _products.Add(new Product() { Id = 3, Name = "Keyboard", Price = 60m });
+        }
+        public void Add(Product product)
+        {
+            product.Id = _products.Any() ? _products.Max(x => x.Id) + 1 : 1;
+            _products.Add(product);
+        }
+
+        public void Remove(Product product)
+        {
+            _products.RemoveAll(x => x.Id == product.Id);
+        }
+
+        public Product Get(int id)
+        {
+            return _products.Where(x => x.Id == id).SingleOrDefault();
+        }
+
+        public List<Product> Get()
+        {
+            return _products.ToList();
+        }
+
+        public void Update(Product product)
+        {
+            var entity = _products.Where(x => x.Id == product.Id).SingleOrDefault();
+
+            if (entity == null)
+            {
+                throw new Exception($"Product with Id not found");
+            }
+            entity.Name = product.Name;
+            entity.Price = product.Price;
+        }
+    }
+}
